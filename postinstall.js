a47750a7ebe8a5664045f55411894ef5b7bee9fb
David Cross (MBP) <david@cloudsociety.com>
2017-04-27 00:54:53 +0800

    Update postinstall with more relevant explanation of how to use Faker. (issue #1)

--- postinstall.js
+++ postinstall.js
@@ -1,4 +1,4 @@
 console.log('Pattern Lab Node Plugin - "plugin-node-faker" installed. ');
 console.log('You may have to run `npm run postinstall` from your console as well.');
 console.log('Configure or disable this plugin inside your patternlab-config.json file.');
-console.log('Add tabs to the Pattern Lab UI by adding file extensions to the "tabsToAdd" array on the plugins.plugin-node-tab.options object.');
+console.log('Add calls to Faker in data.json and listitems.json (ie. "name" : "faker.name.findName"). Quotes are required. Refer to http://marak.github.io/faker.js/ for full list of API methods.');
